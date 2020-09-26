2d613838a395c027d6b1ef2a59a39bad7f40831b
Amar Shankar <amarks444@gmail.com>
2020-09-26 19:40:48 +0530

    Updates

---
+++ precache-manifest.c97d5a2fe1cb20ecaf625c91abb9b201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2b13840bb791ce7e2778aaa49c6cf3",
+    "url": "/amarshankar/index.html"
+  },
+  {
+    "revision": "dcdf5c83a209fdeff45f",
+    "url": "/amarshankar/static/css/main.592d0fa7.chunk.css"
+  },
+  {
+    "revision": "bd65d5e57b21b5205e95",
+    "url": "/amarshankar/static/js/2.e6e4901c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amarshankar/static/js/2.e6e4901c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdf5c83a209fdeff45f",
+    "url": "/amarshankar/static/js/main.7709c80f.chunk.js"
+  },
+  {
+    "revision": "905669381a3b447367b4",
+    "url": "/amarshankar/static/js/runtime-main.96f58935.js"
+  },
+  {
+    "revision": "5b9b4c10fc032a1319918870f2c45e73",
+    "url": "/amarshankar/static/media/amar.5b9b4c10.jpeg"
+  }
+]);
